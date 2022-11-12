478c0b16a24488ca8439defa34d18fafca8209a6
Pablo Barbero <pablobarbero220@gmail.com>
2022-11-12 13:19:26 -0300

    testing component

--- client/src/teams.jsx
+++ client/src/teams.jsx
@@ -1,36 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 
-export const Teams = () => {
+const Teams = () => {
   const getTeams = async () => {
-    const response = await fetch("http://localhost:3001/teams");
+    let response = await fetch("http://localhost:3001/teams");
+    //response = response.sort((a,b) => Number(a.group > b.group)*2-1)
     return response.json();
   };
 
-  const { data, status } = useQuery("", getTeams);
+  const { data, status } = useQuery("teams", getTeams);
 
-  if (status === "loading") {
-    return <p> Loading </p>;
-  }
-
-  if (status === "error") {
-    return <p> Error </p>;
-  }
-
-  const mutatedData = (() => {
-    const dataAsc = data.sort((a, b) => Number(a.group > b.group) * 2 - 1);
-    return dataAsc;
-  })();
   return (
-    <div>
-      <p>TEAMS</p>
-      {mutatedData.map((team) => {
-        return (
-          <div key={team._id}>
-            Country: {team.name} Group: {team.group}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {status === "loading" && <div> Cargando equipos </div>}
+      {status === "error" && <div> Error al cargar los equipos </div>}
+      {status === "success" && (
+        <div>
+          <p>TEAMS</p>
+          {data?.map((team) => {
+            return (
+              <div key={team._id}>
+                Country: {team.name} Group: {team.group}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
+
+export default Teams;
